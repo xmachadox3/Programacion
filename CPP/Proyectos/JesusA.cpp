b706e8195b23994fae1c90148f1440786cb3916b
jesustriker <jesus.m.5262@gmail.com>
2015-07-25 10:34:05 -0430

    update

--- CPP/Proyectos/JesusA.cpp
+++ CPP/Proyectos/JesusA.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
 
 	using namespace std;
-	
+
 	class Pasajero{
 		private:
 			char nombre[25];
@@ -9,48 +11,52 @@
 			char cedula[10];
 			char telefono[15];
 		public:
-			
+
 			void ingresar();
-			void mostrar();	
+			void mostrar();
 			void modificar();
-			//char* id(){return cedula;}		
+			//char* id(){return cedula;}
 			friend ostream& operator << (ostream &o, const Pasajero &p);
 			friend istream& operator >> (istream &i, Pasajero &p);
 	};
-	
+
 	ostream& operator << (ostream &o, const Pasajero &p){
-		o<<"Su Nombre"<<endl;
-		o<<p.nombre<<endl;
-		return o;		
+		o<<"Nombre: "<<p.nombre<<endl;
+		o<<"Apellido: "<<p.apellido<<endl;
+		o<<"Cedula: "<<p.cedula<<endl;
+		o<<"Telefono: "<<p.telefono<<endl;
+		return o;
 	}
-	
+
 	istream& operator >> (istream &i, Pasajero &p){
-			cout<<"Ingrese su nombre"<<endl;
-			i>>p.nombre;
+			cout<<"Ingrese el nombre: "; i>>p.nombre;
+			cout<<"Ingrese el Apellido: "; i>>p.apellido;
+			cout<<"Ingrese la Cedula: "; i>>p.cedula;
+			cout<<"Ingrese el Telefono: "; i>>p.telefono;
 			return i;
 	}
-	
+
 	class Vuelo{
-		bool	rutas[3][20];   //Ruta 1, 2, 3 con sus 20 puestos respectivos 
-		char	idpersona[3][20][10];
+		bool	rutas[3][20];   //Ruta 1, 2, 3 con sus 20 puestos respectivos
+		char	idpersona[3][20];
 		char	nombrerutas[3];
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	int main(){
 		Pasajero pasajero;
 		cin>>pasajero;
